da0cf0534dd3509629b62612056336ee441c9172
Sid <bathibabu.doddi67@gmail.com>
2022-02-24 11:22:48 +0530

    New changes added

--- Retirement/ViewController.swift
+++ Retirement/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+    
+        if Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops", message: "Sorry about that, an error occured.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's cool", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        Analytics.trackEvent("navigated_to_calculator")
+    }
+    
+    @IBAction func click(_ sender: Any) {
+        
+        let current_age : Int? = Int("25")
+        let planned_retirement_age : Int? = Int("35")
+        
+        let properties = ["current_age": String(current_age!),
+                          "planned_retirement_age": String(planned_retirement_age!)]
+        
+        Analytics.trackEvent("calculate_retirement_amount", withProperties: properties)    }
 }
 
